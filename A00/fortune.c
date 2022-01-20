bb52038a55f5f612a90536bb9f103fd4c4af1d44
Grace Choe <gchoe@goldengate.cs.brynmawr.edu>
2022-01-20 15:32:25 +0000

    assignment 0 exercise

---
+++ A00/fortune.c
@@ -0,0 +1,13 @@
+/**
+ * fortune.c
+ * Author: Grace Choe
+ * Date: 1/20/2022
+ * Print Fortune: Always think before you do.
+ */
+
+#include <stdio.h>
+
+int main() {
+ printf("Fortune: Always think before you do.\n");
+ return 0;
+}
