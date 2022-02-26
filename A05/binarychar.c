b5b8d96909dc0d845359616fe6ebc0efa0cf997e
Grace Choe <gchoe@goldengate.cs.brynmawr.edu>
2022-02-26 12:31:28 -0500

    assignment 5 complete

--- A05/binarychar.c
+++ A05/binarychar.c
@@ -1,5 +1,62 @@
+/**
+ * binarychar.c
+ * Author: Grace Choe
+ * Date: 2/24/2022
+ *
+ * Description:
+ * This program implements a program tthat asks the user for an 8-bit binary
+ * number and outputs the corresponding ASCII character (binary to ASCII),
+ * assuming that the user enters a valid 8-bit binary number).
+ */
+
 #include <stdio.h>
 
+//power function
+//Calculates the power with the values: base and exponent.
+//Can use math.h library's pow() by editing the makefile with -lm, but decided
+//to code power instead.
+int power(int base, int exponent) {
+  int answer = 1;
+
+  //for loop, loops through multiplying answer to its base exponent # of times.
+  for (int i = 0; i < exponent; i++) {
+    answer =  answer * base;
+  }
+
+  return answer;
+}
+
+//bintodec function
+//Gets the decimal equivalent of the binary number.
+int bintodec(int bin) {
+  int dec = 0, remainder, pow_val, i = 0;
+ 
+  //while loop, calculates the total decimal value.
+  while (bin != 0) {
+    remainder = bin % 10; //Gets the last binary digit.
+    bin /= 10; //Removes the last binary digit.
+    //Finds the decimal value of the last binary digit using power function.
+    pow_val = remainder * power(2, i);
+    dec += pow_val; //Calculates the decimal value total.
+    i++; //Increments to get the next power of 2.
+  }
+  
+  return dec;
+}
+
+//main function
+//Asks user for 8-digit binary number input, uses bintodec to convert it into
+//decimal. Finds ascii equivalent of the decimal number.
 int main() {
+  int binary, decimal;
+
+  printf("Enter 8 bits: ");
+  scanf("%d", &binary);
+
+  decimal = bintodec(binary);
+  //Since %c and %d are interchangeable within the ascii, printing out the %c
+  //version of int decimal gives decimal's ascii char equivalent.
+  printf("Your character is: %c\n", decimal);
+  
   return 0;
 }
--- A05/word.c
+++ A05/word.c
@@ -1,5 +1,51 @@
-#include <stdio.h>
+/**
+ * word.c
+ * Author: Grace Choe
+ * Date: 2/23/2022
+ *
+ * Description:
+ * This program asks the user for a four letter word and stores the characters
+ * within a single unsigned int, assuming that the user enters a valid four
+ * letter word. Debugs are also printed out and kept like the example output
+ * given. The debugs prints the hexadecimal values of each character, shifted to
+ * its correct spot.
+ */
 
+#include <stdio.h>
+#include <string.h>
+
+//main function
 int main() {
+  //string char array to hold the four letter word user input.
+  char string[4];
+  //Holds the char version of each letter's hex value.
+  char hex[9];
+  //num holds the hex formatted version of each letter's hex value.
+  //shifting holds the correctly shifter version of each letter's hex value.
+  unsigned int num, shifting;
+  unsigned int hex_add = 0x00000000;
+  int shift = 0;
+
+  printf("Enter 4 characters: ");
+  scanf(" %s", string);
+
+  for (int k = 3; k >= 0; k--) {
+    //Reformats the specified letter of string into hex format.
+    sprintf(hex, "000000%x", string[k]);
+    //Transfers hex into %08X formatted unsigned int num.
+    sscanf(hex, "%08X", &num);
+    
+    //Shifts the hex value to its correctly shifted spot.
+    shifting  = num << shift;
+    shift += 8;
+    
+    //Adds the shifted value to hex_add which holds the final single unsigned
+    //int for the word's hex value.
+    hex_add += shifting;
+    printf("Debug: %c = 0x%08X\n", string[k], shifting);
+  }
+
+  printf("Your number is: %d (0x%08X)\n", hex_add, hex_add);
+
   return 0;
 }
