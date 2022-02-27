6912ec9443c4c70388a6df6335618ebf7ee972f8
Grace Choe <gchoe@goldengate.cs.brynmawr.edu>
2022-02-26 23:47:32 -0500

    assignment 5 complete

--- A05/glitch.c
+++ A05/glitch.c
@@ -13,6 +13,7 @@
 #include <string.h>
 #include "read_ppm.h"
 
+//main function
 int main(int argc, char** argv) {
   struct ppm_pixel **array;
   int width = 0;
@@ -29,14 +30,18 @@ int main(int argc, char** argv) {
     exit(1);
   }
   
+  //Uses the read_ppm function to read the file and make the array.
   array = read_ppm(argv[1], &width, &height);
   printf("Reading %s with width %d and height %d\n", argv[1], width, height); 
   
+  //Creates token to make new file name.
   token = strtok(argv[1], period);
   char name[] = "-glitch.ppm";
   
+  //Creates new file name.
   strcat(token, name);
   
+  //Edits the array to "glitch" the pixel colors.
   for (int i = 0; i < height; i++) {
     for (int j = 0; j < width; j++) {
       for (int m = 0; m < 3; m++) {
@@ -61,14 +66,17 @@ int main(int argc, char** argv) {
     }
   }
   
+  //Uses the write_ppm function to write a new file with the "glitches".
   write_ppm(token, array, width, height);
   printf("Writing file %s\n", token);
   
+  //Frees the arrays within the array (2D array)
   for (int k = 0; k < height; k++) {
     free(array[k]);
     array[k] = NULL;
   }
   
+  //Frees the final outer array.
   free(array);
   array = NULL;
   
