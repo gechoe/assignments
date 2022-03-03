e722c58f2d89c1984271b9e398fa0f27469e6b7b
Grace Choe <gchoe@goldengate.cs.brynmawr.edu>
2022-03-03 02:13:33 -0500

    J02 edit complete

--- J02/linkedlist.c
+++ J02/linkedlist.c
@@ -1,5 +1,16 @@
+/*
+ * linkedlist.c
+ * Author: Grace Choe
+ * Date: 3/1/2022
+ * 
+ * Note:
+ * I remade this linkedlist program from Code Jam 2 to make it work correctly
+ * in terms of the Code Jam's guidelines. 
+ */
+
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 struct cake {
   float cost;
@@ -16,16 +27,41 @@ struct cake makeCake(const char* flavor, float cost) {
 }
 
 // todo: implement cheapestCake
+struct cake* cheapestCake(struct cake* first) {
+  struct cake* curr = first;
+  struct cake* cheap = first;
+
+  while (curr != NULL) {
+    if (curr->cost < cheap ->cost) {
+      cheap = curr;
+    }
+    
+    curr = curr->next;
+  }
+
+  return cheap;
+}
 
 int main() {
   struct cake cake1 = makeCake("red velvet", 2.00);
   struct cake cake2 = makeCake("chocolate", 1.75);
   struct cake cake3 = makeCake("mocha", 3.50);
+  struct cake cake4 = makeCake("vanilla", 5.00);
   // todo: define 4th cake
 
   cake1.next = &cake2;
   cake2.next = &cake3;
+  cake3.next = &cake4;
   // draw stack and heap here
 
+  struct cake* current = &cake1;
+
+  while (current != NULL) {
+    printf("cake: %s (%.2f)\n", current->flavor, current->cost);
+    current = current->next;
+  }
+  
   // todo: call cheapestCake
+  struct cake* cheapest = cheapestCake(&cake1);
+  printf("The cheapest cake is %s\n", cheapest->flavor);
 }
--- J02/readvector.c
+++ J02/readvector.c
@@ -1,12 +1,50 @@
+/*
+ * readvector.c
+ * Author: Grace Choe
+ * Date: 3/1/2022
+ * 
+ * Note:
+ * I remade this readvector program from Code Jam 2 to make it work correctly
+ * in terms of the Code Jam's guidelines. 
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // readvector opens and reads the given file
 // The size of the vector should be stored in the parameter size
 // The values should be returned in a flat float array
 float* readvector(const char* filename, int *size) {
   // todo: implement me
-  return NULL;
+  float *f_array = NULL;
+  
+  FILE *file;
+
+  file = fopen(filename, "r");
+
+  if (file == NULL) {
+    printf("Error: Unable to open file %s\n", filename);
+    exit(1);
+  }
+
+  fscanf(file, "%d", size);
+
+  f_array = malloc(sizeof(float) * (*size));
+
+  if (f_array == NULL) {
+    printf("Error: malloc failed!\n");
+    exit(1);
+  }
+ 
+  for (int i = 0; i < *size; i++) {
+    f_array[i] = '\0';
+    fscanf(file, "%f", &f_array[i]);
+  }
+  
+  fclose(file);
+
+  return f_array;
 }
 
 int main(int argc, char** argv) {
@@ -19,8 +57,14 @@ int main(int argc, char** argv) {
   float* vector = readvector(argv[1], &size);
 
   // todo: print vector values
+  for (int i = 0; i < size; i++) {
+    if (vector[i] != '\0') {
+      printf("%.6f\n", vector[i]);
+    }
+  }
 
   free(vector);
+  vector = NULL;
+
   return 0;
 }
-
