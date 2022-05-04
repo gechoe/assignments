41e8baf966069bbcba9f9be04c9d9e58568f9a57
Grace Choe <gchoe@goldengate.cs.brynmawr.edu>
2022-05-04 12:45:15 -0400

    assignment 9 edit complete

--- A09/multi_mandelbrot.c
+++ A09/multi_mandelbrot.c
@@ -1,6 +1,15 @@
+/**
+ * multi_mandelbrot.c
+ * Author: Grace Choe
+ * Date: 4/15/2022
+ *
+ * Description:
+ * This program writes a mandelbrot png file using multiple (four) child 
+ * processes using fork.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <assert.h>
 #include <time.h>
@@ -10,8 +19,10 @@
 #include <sys/ipc.h>
 #include "read_ppm.h"
 
-struct ppm_pixel** make_array(struct ppm_pixel* pal, struct ppm_pixel** arr, 
-  int startr, int endr, int startc, int endc) {
+//make_array function
+//Creates the mandelbrot array by filling it with colors
+void make_array(struct ppm_pixel** arr, struct ppm_pixel* pal,
+  int startrow, int endrow, int startcol, int endcol) {
   int size = 480;
   float xmin = -2.0;
   float xmax = 0.47;
@@ -23,16 +34,16 @@ struct ppm_pixel** make_array(struct ppm_pixel* pal, struct ppm_pixel** arr,
 
   //For loop within for loop to assign red, green, blue pixel colors for each
   //pixel
-  for (int j = startr; j < endr; j++) {
-    for (int k = startc; k < endc; k++) {
-      float xfrac = (float) k / (float)size;
-      float yfrac = (float) j / (float)size;
-      
+  for (int r = startrow; r < endrow; r++) {
+    for (int c = startcol; c < endcol; c++) {
+      float xfrac = (float) c / (float)size;
+      float yfrac = (float) r / (float)size;
+        
       float x0 = xmin + xfrac * (xmax - xmin);
       float y0 = ymin + yfrac * (ymax - ymin);
       float x = 0, y = 0;
       int iter = 0;
-      
+
       //while loop creates the fractal like image by assigning a certain iter
       //value which is then used to compute the pixel colors
       while ((iter < maxIterations) && ((x * x + y * y) < (2 * 2))) {
@@ -52,15 +63,18 @@ struct ppm_pixel** make_array(struct ppm_pixel* pal, struct ppm_pixel** arr,
         colorg = black;
         colorb = black;
       }
-
-      //write color to image at location (row, col)
-      arr[j][k].red = colorr;
-      arr[j][k].green = colorg;
-      arr[j][k].blue = colorb;
+      
+      arr[r][c].red = colorr;
+      arr[r][c].green = colorg;
+      arr[r][c].blue = colorb;
     }
   }
- 
-  return arr;
+
+  free(pal);
+  pal = NULL;
+
+  free(arr);
+  arr = NULL;
 }
 
 int main(int argc, char* argv[]) {
@@ -84,31 +98,17 @@ int main(int argc, char* argv[]) {
         "-b <ymin> -t <ymax> -p <numProcesses>\n", argv[0]); break;
     }
   }
-  printf("Generating mandelbrot with size %dx%d\n", size, size);
+
+  printf("Generating image with size %dx%d\n", size, size);
   printf("  Num processes = %d\n", numProcesses);
   printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
 
   // todo: your code here
-  char name[200] = "multi-mandelbrot-";
-  char *dash = "-";
-  char sizelet[100];
-  sprintf(sizelet, "%d", size);
-  char ppm[] = ".ppm";
-  time_t timenow = time(0);
-  char current[11];
-  sprintf(current, "%d", (int)timenow);
-  
-  //Creates file name
-  strcat(name, sizelet);
-  strcat(name, dash);
-  strcat(name, current);
-  strcat(name, ppm);
-
   // generate pallet
   //Sets a random seed to ensure that the color palette is different each time
-  srand(time(0));  
-  
+  srand(time(0));
+
   //palette array to hold palette colors
   struct ppm_pixel *palette;
   palette = malloc(sizeof(struct ppm_pixel *) * (maxIterations + 20));
@@ -129,54 +129,40 @@ int main(int argc, char* argv[]) {
     palette[q].blue = blue; 
   }
 
-  // compute image
-  //Creating space for shared memory to hold 2d array for child processes 
-  int shmid, shmid2;
-  shmid = shmget(IPC_PRIVATE, sizeof(struct ppm_pixel*) * (size + 100), 0644 | IPC_CREAT);
-
+  //shared memory initialized
+  int shmid;
+  shmid = shmget(IPC_PRIVATE, sizeof(struct ppm_pixel) * size *size, 0644 | IPC_CREAT);
   if (shmid == -1) {
-    perror("Error: Cannot initialize shared memory!\n");
-    exit(1);
-  }
- 
-  //2d array to hold the mandelbrot fractal's pixel values 
-  struct ppm_pixel** array = shmat(shmid, NULL, 0);
-  
-  if (*array == (void*) -1) {
-    perror("Error: cannot access shared memory\n");
+    perror("Error: cannot initialize shared memory\n");
     exit(1);
   }
 
-  for (int w = 0; w < size; w++) {
-    shmid2 = shmget(IPC_PRIVATE, sizeof(struct ppm_pixel) * (size + 100), 0644 | IPC_CREAT);
-    array[w] = shmat(shmid2, NULL, 0);
-    if (shmid == -1) {
-      perror("Error: Cannot initialize shared memory!\n");
-      exit(1);
-    }
-  }
-  
-  if (array == (void*) -1) {
+  //shared memory accessed
+  struct ppm_pixel* buffer = shmat(shmid, NULL, 0);
+  if (buffer == (void*) -1) {
     perror("Error: cannot access shared memory\n");
     exit(1);
+  } 
+
+  //Creates 2d array using the shared memory space
+  struct ppm_pixel** image_array = malloc(sizeof(struct ppm_pixel*) * size);
+  for (int i = 0; i < size; i++) {
+    image_array[i] = &(buffer[i*size]); 
   }
-  
-  //double timer;
+
+  //timer
   struct timeval tstart, tend;
+  int startr, endr, startc, endc;
 
   //The start time, gets the time for before the mandelbrot (array/png) is made
   gettimeofday(&tstart, NULL);
 
-  int colorr, colorg, colorb, black = 0;
-  float xtmp = 0;
-
-  int startr, endr, startc, endc;
-
+  //for loop to create four forked child processes which create the mandelbrot
   for (int y = 0; y < 4; y++) {
     int pid = fork();
-    
+
     if (pid == -1) {
-      printf("Error: fork failed\n");
+      printf("Error: Fork failed!\n");
       exit(pid);
     }
 
@@ -191,7 +177,7 @@ int main(int argc, char* argv[]) {
     }
 
     if (pid == 0) {
-      array = make_array(palette, array, startr, endr, startc, endc);
+      make_array(image_array, palette, startr, endr, startc, endc);
       exit(0);
     } else {
       printf("Launched child process: %d\n", pid);
@@ -199,7 +185,7 @@ int main(int argc, char* argv[]) {
         pid, startr, endr, startc, endc);
     }
   }
-
+  
   for (int z = 0; z < 4; z++) {
     int status;
     int pid = wait(&status);
@@ -210,39 +196,29 @@ int main(int argc, char* argv[]) {
   //The end time, gets the time for after the mandelbrot array is made
   gettimeofday(&tend, NULL);
 
-  //Write the computed mandelbrot into a png file
-  write_ppm(name, array, size, size);
-  
-  for (int m = 0; m < size; m++) {
-    if (shmdt(array[m]) == -1) {
-      perror("Error: Cannot detach from shared memory!\n");
-      exit(1);
-    }
-  }
-
-  if (shmctl(shmid2, IPC_RMID, 0) == -1) {
-    perror("nott Error: Cannot remove shared memory!\n");
-    exit(1);
-  }
-
-  if(shmdt(array) == -1) {
-    perror("huh Error: Cannot detach from shared memory!\n");
-    exit(1);
-  }
-
-  if (shmctl(shmid, IPC_RMID, 0) == -1) {
-    perror("ok Error: Cannot remove shared memory!\n");
-    exit(1);
-  }
-
-  //timer, calculates the total time the process took
-  double timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
-  printf("Computer mandelbrot set (%dx%d) in %g seconds\n", size, size, timer);
+  //name array holds the created name of the file
+  char name[1024];
+  snprintf(name, 1024, "multi-mandelbrot-%d-%lu.ppm", size, time(0));
+  write_ppm(name, image_array, size, size);
   printf("Writing file: %s\n", name);
   
-  //Frees the palette array
+  //frees image_array
+  free(image_array);
+  image_array = NULL;
+
+  //Makes sure shared memory is detached
+  if (shmdt(buffer) == -1) {
+    perror("Error: cannot detatch from shared memory\n");
+    exit(1);
+  }
+
+  //Makes sure shared memory is removed
+  if (shmctl(shmid, IPC_RMID, 0) == -1) {
+    perror("Error: cannot remove shared memory\n");
+    exit(1);
+  }
+
+  //frees palette
   free(palette);
   palette = NULL;
-
-  return 0;
 }
